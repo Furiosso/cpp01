b4d693d3fadb89a9f2dcd2ed5b14683240b2f70f
Furiosso <dgimenodemanuel@gmail.com>
2025-08-14 11:27:55 +0200

    delete main.hpp

--- ex00/Zombie.hpp
+++ ex00/Zombie.hpp
@@ -14,4 +14,7 @@ private:
 	std::string	_name;
 };
 
+Zombie	*newZombie(std::string name);
+void	randomChump(std::string name);
+
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,8 +1,9 @@
-#include "main.hpp"
+#include "Zombie.hpp"
 
 int	main(void)
 {
 	Zombie *pepe = newZombie("Pepe");
+
 	randomChump("Sergio");
 	pepe->announce();
 	delete(pepe);
