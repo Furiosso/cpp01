d184e02875c41504f6e7a120bd5aee4e19fac124
Furiosso <dgimenodemanuel@gmail.com>
2025-08-18 08:58:15 +0200

    correction ex03

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -11,5 +11,8 @@ void	HumanB::setWeapon(Weapon &weapon)
 
 void	HumanB::attack(void)
 {
-	std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	if (this->_weapon)
+		std::cout << this->_name << " attacks with their " << this->_weapon->getType() << std::endl;
+	else
+		std::cout << this->_name << " attacks with their " << std::endl;
 }
